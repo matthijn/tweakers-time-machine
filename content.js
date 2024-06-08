6f5578d1ea568bfe16071492780a8068d44dae26
Matthijn Dijkstra <matthijndijkstra@gmail.com>
2024-06-08 13:26:29 +0200

    feat(cache): Add caching support
    Add caching support
    
    Signed-off-by: Matthijn Dijkstra

--- content.js
+++ content.js
@@ -1,9 +1,13 @@
-import {randomized_articles} from "./domain/time_machine";
+import {fetch_articles, randomized_articles} from "./domain/time_machine";
 import {append_historical_news} from "./domain/modify_news_dom";
+import {end_of_day} from "./lib/time";
+import {make_cacheable} from "./lib/cache";
 
 async function main() {
-    const articles = await randomized_articles()
-    await append_historical_news(articles)
+    const cached_article_call = make_cacheable(fetch_articles, "historical_articles", end_of_day())
+    const all_articles = await cached_article_call()
+    const selection = randomized_articles(all_articles)
+    await append_historical_news(selection)
 }
 
 main().catch(console.error)
