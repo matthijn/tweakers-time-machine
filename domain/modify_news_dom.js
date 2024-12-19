f94687d2b5cca9acf7ea5119a3b0c989a8acbdf8
Peter de Bruine <pdebruine@ecisolutions.com>
2024-12-19 14:34:04 +0100

    Sidebar class was changed.

--- domain/modify_news_dom.js
+++ domain/modify_news_dom.js
@@ -5,7 +5,7 @@ export function append_historical_news(articles, display_location) {
     const elements = articles_to_dom_elements(articles)
 
     if (display_location === NewsLocation.SIDEBAR) {
-      const target = document.querySelector(".sidebar-1");
+      const target = document.querySelector(".sidebar");
       target.prepend(elements);
     } else {
       const target = document.querySelector(".headline-block-2");
